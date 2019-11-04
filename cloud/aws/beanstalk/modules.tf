5c1a0c2a922844d31dfe74ce6ad19327d17b5624
Charles Vauvarin <charles.vauvarin@oxalide.com>
2019-11-04 17:15:04 +0100

    MON-525 : Remove dirty module path fix

--- cloud/aws/beanstalk/modules.tf
+++ cloud/aws/beanstalk/modules.tf
@@ -1,6 +1,5 @@
 module "filter-tags" {
-  #source = "../../../common/filter-tags"
-  source = "/home/cvauvarin/work/src/git.fr.clara.net/claranet/pt-monitoring/projects/datadog/terraform/monitors/common/filter-tags"
+  source = "../../../common/filter-tags"
 
   environment                 = var.environment
   resource                    = "aws_beanstalk"
