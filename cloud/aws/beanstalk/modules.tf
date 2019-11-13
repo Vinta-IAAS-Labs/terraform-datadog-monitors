5b6641921542c751468728973924c7601eed9a2b
Charles Vauvarin <charles.vauvarin@oxalide.com>
2019-11-13 11:43:16 +0100

    MON-525 : add comment on the specific filter module call

--- cloud/aws/beanstalk/modules.tf
+++ cloud/aws/beanstalk/modules.tf
@@ -8,8 +8,19 @@ module "filter-tags" {
   filter_tags_custom_excluded = var.filter_tags_custom_excluded
 }
 
-# As some metrics are send per host and per beanstalk env
-# we need to do some exclusion to alert on the right value
+# With AWS beanstalk some metrics are send per host and per beanstalk env.
+# This is particularly the case for all the ApplicationLatency metrics and
+# the ApplicationRequests (not for the health and the cpu/disk metrics).
+# The best way to find this out is to go on the monitoring configuration page
+# of your beanstalk environment.
+#
+# In order to differentiate those metrics we need to do some exclusion to
+# to find out which values has been sent for the host and the one sent for
+# the environment itself.
+# Some automatic tags are added on the instances by AWS, this seems to be
+# the only way to filter at the moment.
+#
+# This filter exclude the metrics sent for the hosts.
 module "filter-tags-no-host" {
   source = "../../../common/filter-tags"
 
